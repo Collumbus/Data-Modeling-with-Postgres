63bacf59749c84fdbd7c46b2ccf6f931e6cf97c2
Jorge Luiz <jorgeluizjk@gmail.com>
2021-12-09 10:16:25 -0300

    chore: Implements etl.ipynb and sql_queies.py

--- sql_queries.py
+++ sql_queries.py
@@ -1,52 +1,108 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays(
+        songplay_id SERIAL PRIMARY KEY,                     -- Sets primary key serially
+        start_time TIMESTAMP REFERENCES time (start_time),  -- Sets the foreign key related to table time
+        user_id INT REFERENCES users (user_id),             -- Sets the foreign key related to table users
+        level VARCHAR,
+        song_id VARCHAR REFERENCES songs (song_id),         -- Sets the foreign key related to table songs
+        artist_id VARCHAR REFERENCES artists (artist_id),   -- Sets the foreign key related to table artists
+        session_id INT, 
+        location TEXT, 
+        user_agent TEXT)
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INT PRIMARY KEY,                            -- Sets primary key
+        first_name VARCHAR,
+        last_name VARCHAR,
+        gender CHAR(1),
+        level VARCHAR)
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id VARCHAR PRIMARY KEY,                        -- Sets primary key
+        title VARCHAR,
+        artist_id VARCHAR REFERENCES artists (artist_id),   -- Sets the foreign key related to table artists
+        year INT,
+        duration FLOAT)
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id VARCHAR PRIMARY KEY,                        -- Sets primary key,
+        name VARCHAR,
+        location TEXT,
+        latitude FLOAT,
+        longitude FLOAT)
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time TIMESTAMP PRIMARY KEY,
+        hour INT,
+        day INT,
+        week INT,
+        month INT,
+        year INT,
+        weekday VARCHAR)
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+    INSERT INTO users (user_id, first_name, last_name, gender, level)
+    VALUES (%s, %s, %s, %s, %s)
+     ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id) DO NOTHING
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists (artist_id, name, location, latitude, longitude)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO NOTHING
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) DO NOTHING
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT 
+            s.song_id,
+            a.artist_id
+    FROM songs s
+    JOIN artists a ON s.artist_id = a.artist_id
+    WHERE s.title = %s AND a.name = %s AND s.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [artist_table_create, song_table_create, user_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
